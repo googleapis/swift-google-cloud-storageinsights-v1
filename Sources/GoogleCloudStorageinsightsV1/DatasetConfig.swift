7e03d9fa52bb3e9dc4f6df04892edc6c1b0c8e6c
Carlos O'Ryan <coryan@google.com>
2026-07-27 18:20:35 -0400

    chore: update librarian circa 2026-07-27 (#48)
    In this version, librarian generates correct cross-reference links for
    enums. This fixes a number of documentation warnings for
    `google-cloud-wkt` and many generated libraries.

--- Sources/GoogleCloudStorageinsightsV1/DatasetConfig.swift
+++ Sources/GoogleCloudStorageinsightsV1/DatasetConfig.swift
@@ -857,7 +857,7 @@ public struct DatasetConfig: Codable, Equatable, GoogleCloudWkt._AnyPackable,
 
     /// Initialize from a string value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownStringValue`](doc:ConfigState/unknownStringValue(_:)).
     public init(stringValue: Swift.String) {
       switch stringValue {
       case "CONFIG_STATE_UNSPECIFIED": self = .unspecified
@@ -871,7 +871,7 @@ public struct DatasetConfig: Codable, Equatable, GoogleCloudWkt._AnyPackable,
 
     /// Initialize from an integer value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownIntValue`](doc:ConfigState/unknownIntValue(_:)).
     public init(intValue: Int) {
       switch intValue {
       case 0: self = .unspecified
--- Sources/GoogleCloudStorageinsightsV1/FrequencyOptions.swift
+++ Sources/GoogleCloudStorageinsightsV1/FrequencyOptions.swift
@@ -104,7 +104,7 @@ public struct FrequencyOptions: Codable, Equatable, GoogleCloudWkt._AnyPackable,
 
     /// Initialize from a string value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownStringValue`](doc:Frequency/unknownStringValue(_:)).
     public init(stringValue: Swift.String) {
       switch stringValue {
       case "FREQUENCY_UNSPECIFIED": self = .unspecified
@@ -116,7 +116,7 @@ public struct FrequencyOptions: Codable, Equatable, GoogleCloudWkt._AnyPackable,
 
     /// Initialize from an integer value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownIntValue`](doc:Frequency/unknownIntValue(_:)).
     public init(intValue: Int) {
       switch intValue {
       case 0: self = .unspecified
